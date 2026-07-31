ace495617e88696ad5fe29aff3ae0e67ae902f40
lilei <i@lilei.in>
2026-07-31 18:48:28 +0800

    Fix aurora branch name and completely remove legacy dependencies for turboacc and eqos

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -2,4 +2,4 @@
 # diy-part1.sh
 
 # 添加 Aurora 主题到 feeds
-echo "src-git aurora https://github.com/eamonxg/luci-theme-aurora.git;main" >> feeds.conf.default
+echo "src-git aurora https://github.com/eamonxg/luci-theme-aurora.git;master" >> feeds.conf.default
