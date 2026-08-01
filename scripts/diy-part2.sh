e799a0fda41f6eed60cd7dcaa7eb19dc4505f9bd
lilei <i@lilei.in>
2026-08-02 06:50:28 +0800

    Add cmcc_rax3000m-emmc target definition to build sysupgrade.itb for U-Boot mod layout

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -49,3 +49,28 @@ sed -i 's/+lua-cjson //g' package/mtk/applications/mtwifi-cfg/Makefile || true
 sed -i 's/+lua-cjson//g' package/mtk/applications/mtwifi-cfg/Makefile || true
 sed -i 's/+ebtables-legacy-utils//g' package/mtk/applications/luci-app-eqos-mtk/Makefile || true
 sed -i 's/+ebtables-legacy//g' package/mtk/applications/luci-app-eqos-mtk/Makefile || true
+
+# 添加 cmcc_rax3000m-emmc (U-Boot mod 布局，生成 sysupgrade.itb 固件)
+cat << 'EOF' >> target/linux/mediatek/image/filogic.mk
+
+define Device/cmcc_rax3000m-emmc
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := RAX3000M EMMC
+  DEVICE_VARIANT := (U-Boot mod)
+  DEVICE_DTS := mt7981b-cmcc-rax3000m-emmc-mtk
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3 \
+	automount f2fsck mkf2fs
+  SUPPORTED_DEVICES += cmcc,rax3000m-emmc
+  KERNEL_LOADADDR := 0x44000000
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  IMAGES := sysupgrade.itb
+  IMAGE/sysupgrade.itb := append-kernel | \
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | \
+	pad-rootfs | append-metadata
+endef
+TARGET_DEVICES += cmcc_rax3000m-emmc
+EOF
